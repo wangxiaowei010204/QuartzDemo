d547ce73026305822e438174efcf892c2e302b94
wangxiaowei010204 <844503316@qq.com>
2020-05-24 13:37:17 +0800

    quartzDemo

---
+++ QuartzDemo/QuartzDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace QuartzDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestTask.StartTestAsync().GetAwaiter();
+
+            Console.ReadKey();
+        }
+    }
+}
